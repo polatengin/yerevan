72a259455dcc8990128e806bfbc4e9c6dac372b0
Engin Polat <enpolat@microsoft.com>
2023-03-10 23:13:56 +0000

    adding links to products and users pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +13,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav>
+          <ul>
+            <li><Link href={"/products"}>Products</Link></li>
+            <li><Link href={"/users"}>Users</Link></li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
