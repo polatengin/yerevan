78a90f91fd2a39f7b375a13e8aed5f126c1b9b6e
Engin Polat <enpolat@microsoft.com>
2023-03-11 00:15:14 +0000

    attaching to login channel and get the ref of the channel

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { useBroadcastChannel } from "@/useBroadcastChannel";
+
 const generateUserName = () => {
   const arr = new Uint8Array(4);
   window.crypto.getRandomValues(arr)
   return Array.from(arr, (dec) => dec.toString(16).padStart(2, "0")).join('');
 };
+
 export default function Products() {
+  const postLoginChanged = useBroadcastChannel("login");
+
   return (
     <div>
       <h1>Products</h1>
